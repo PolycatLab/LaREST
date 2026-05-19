de20b49518a22d62571382f711127b44d6f6168a
utf <alexganose@googlemail.com>
2026-05-19 13:00:46 +0100

    Update pipeline

--- pipeline.sh
+++ pipeline.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/bash
 #PBS -N LaREST
 #PBS -l walltime=23:59:00
-#PBS -l select=1:ncpus=128:mem=512gb:mpiprocs=128
+#PBS -l select=1:ncpus=16:mem=512gb:mpiprocs=16
 
 # MODIFY ME
 # ---------
-CONDA_DIR="${HOME}/miniforge3/bin"
+CONDA_DIR="${HOME}/anaconda3/bin"
 CONDA_ENV="larest"
-N_CORES=128
+N_CORES=16
 # ---------
 
 OUTPUT_DIR="output"
-CONFIG_FILE="config/config.toml"
+CONFIG_FILE="config.toml"
 
 # xtb-required options
 ulimit -s unlimited
@@ -22,12 +22,14 @@ export OMP_MAX_ACTIVE_LEVELS=1
 export OPENBLAS_NUM_THREADS=1
 export XTBPATH="$(pwd)"
 
-# load orca and censo
-module load ORCA/6.1.0-gompi-2023b
 
 # activate conda env
 eval "$("${CONDA_DIR}"/conda shell.bash hook)"
 conda activate ${CONDA_ENV}
+
+# load orca and censo and crest
+module load tools/prod
+module load ORCA/6.1.1-gompi-2024a-avx2 CREST/3.0.2-gfbf-2023b
 
 # create run directory
 mkdir -p "${PBS_O_WORKDIR}/${OUTPUT_DIR}"
